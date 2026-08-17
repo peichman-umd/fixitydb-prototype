71d36a8de37fbdb84e984d1b90c554b1a616c3a4
Peter Eichman <peichman@umd.edu>
2026-08-16 21:23:59 -0400

    Added the BaseTask with on_failure callback

--- src/postgrest/tasks.py
+++ src/postgrest/tasks.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 
-from celery import Celery, group
+from celery import Celery, group, Task
 from celery.utils.log import get_task_logger
 from dotenv import load_dotenv
 
@@ -17,32 +17,33 @@ records = FixityRecords.from_config(os.environ)
 app = Celery('fixity', broker=os.environ['CELERY_BROKER'], backend=os.environ['CELERY_BACKEND'])
 
 
-#def on_failure(self, exc, task_id, args, kwargs, einfo):
-#    """This means the fixity check did NOT run successfully (e.g., there was a network
-#    communication error). In this case, it will notify DPI and the developers."""
-#    logger.error(f'unable to run fixity check! (args: {args}): {exc}')
-#    logger.error('TODO: notify DPI and developers')
+class BaseTask(Task):
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        """This means the fixity check did NOT run successfully (e.g., there was a network
+        communication error). In this case, it will notify DPI and the developers."""
+        logger.error(f'unable to run task {self.name}[{task_id}] (args: {args}, kwargs: {kwargs}): {exc}')
+        logger.error('TODO: notify DPI and developers')
 
 
-@app.task()
+@app.task(base=BaseTask)
 def check_least_recent(batch_size: int = 250):
     logger.debug(f'Checking least-recent fixity for {batch_size} records')
     group(check_and_record_fixity.s(uri) for uri in records.get_least_recent(batch_size))()
 
 
-@app.task()
+@app.task(base=BaseTask)
 def check_and_record_fixity(uri: str):
     chain = check_fixity.s(uri) | record_result.s()
     chain()
 
 
-@app.task()
+@app.task(base=BaseTask)
 def check_fixity(uri: str):
     logger.info(f'Checking {uri}')
     return records.check(uri)
 
 
-@app.task()
+@app.task(base=BaseTask)
 def record_result(result: dict[str, str | int]):
     fixity_success = result['outcome'] == 'SUCCESS'
     logger.debug(json.dumps(result, indent=None))
